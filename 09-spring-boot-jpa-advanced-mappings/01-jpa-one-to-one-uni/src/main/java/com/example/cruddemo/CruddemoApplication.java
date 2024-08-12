935b7425567425f40c4561bbb740ac363163e7c7
Sl0wdive <74788692+Sl0wdive@users.noreply.github.com>
2024-08-12 16:38:55 +0300

    updated

---
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/example/cruddemo/CruddemoApplication.java
@@ -0,0 +1,82 @@
+package com.example.cruddemo;
+
+import com.example.cruddemo.dao.AppDAO;
+import com.example.cruddemo.entity.Instructor;
+import com.example.cruddemo.entity.InstructorDetail;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AppDAO appDAO)
+	{
+		return runner -> {
+			// createInstructor(appDAO);
+
+//			findInstructor(appDAO);
+			deleteInstructor(appDAO);
+		};
+	}
+
+	private void deleteInstructor(AppDAO appDAO) {
+		int theId = 1;
+		System.out.println("Deleting instructor id:" + theId);
+
+		appDAO.deleteInstructorById(theId);
+
+		System.out.println("Done!");
+	}
+
+	private void findInstructor(AppDAO appDAO) {
+		int theId = 2;
+		System.out.println("Finding instructor id: " + theId);
+
+		Instructor tempInstructor = appDAO.findInstructorById(theId);
+
+		System.out.println("tempInstructor: " + tempInstructor);
+		System.out.println("the associated instructorDetail only: " + tempInstructor.getInstructorDetail());
+	}
+
+	private void createInstructor(AppDAO appDAO) {
+
+		/*
+		Instructor tempInstructor =
+				new Instructor("Vlad", "Hobona","vladhobona@gmail.com");
+
+
+		InstructorDetail tempInstructorDetail =
+				new InstructorDetail(
+						"http://example.com",
+						"Codding"
+				);
+
+
+		 */
+
+
+		Instructor tempInstructor =
+				new Instructor("Vika", "Hobona","vikaggobona@gmail.com");
+
+
+		InstructorDetail tempInstructorDetail =
+				new InstructorDetail(
+						"http://example.com",
+						"Guitar"
+				);
+
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		System.out.println("Saving instructor: " + tempInstructor);
+		appDAO.save(tempInstructor);
+
+	}
+}
