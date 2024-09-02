96a4ff84612d43932939da084afe966ec1f44778
Sl0wdive <74788692+Sl0wdive@users.noreply.github.com>
2024-09-02 23:38:03 +0300

    update: 344

---
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/example/aopdemo/AopdemoApplication.java
@@ -0,0 +1,37 @@
+package com.example.aopdemo;
+
+import com.example.aopdemo.dao.AccountDAO;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AopdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopdemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO) {
+
+		return runner -> {
+			demoTheBeforeAdvice(theAccountDAO);
+		};
+	}
+
+	private void demoTheBeforeAdvice(AccountDAO theAccountDAO) {
+
+		theAccountDAO.addAccount();
+
+		System.out.println("let`s call it again!\n");
+
+		theAccountDAO.addAccount();
+	}
+}
+
+
+
+
+
