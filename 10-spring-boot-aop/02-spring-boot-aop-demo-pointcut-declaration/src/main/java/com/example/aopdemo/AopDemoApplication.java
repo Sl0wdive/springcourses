596a0f81bbcb2ed0e6a54aa89af2570c17eb3934
Sl0wdive <74788692+Sl0wdive@users.noreply.github.com>
2024-09-07 22:15:04 +0300

    update: 364

---
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declaration/src/main/java/com/example/aopdemo/AopDemoApplication.java
@@ -0,0 +1,46 @@
+package com.example.aopdemo;
+
+import com.example.aopdemo.dao.AccountDAO;
+import com.example.aopdemo.dao.MembershipDAO;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AopDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopDemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO, MembershipDAO theMembershipDAO) {
+
+		return runner -> {
+			demoTheBeforeAdvice(theAccountDAO, theMembershipDAO);
+		};
+	}
+
+	private void demoTheBeforeAdvice(AccountDAO theAccountDAO, MembershipDAO theMembershipDAO) {
+
+		Account myAccount = new Account();
+		theAccountDAO.addAccount(myAccount, true);
+		theAccountDAO.doWork();
+
+		theAccountDAO.setName("foobar");
+		theAccountDAO.setServiceCode("silver");
+
+		String name = theAccountDAO.getName();
+		String code = theAccountDAO.getServiceCode();
+
+		theMembershipDAO.addSillyMember();
+		theMembershipDAO.goToSleep();
+
+	}
+}
+
+
+
+
+
